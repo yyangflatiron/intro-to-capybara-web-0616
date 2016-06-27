521db94710c9746ab1a717b25ce21a6e1679fdf9
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-27 19:24:23 -0400

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,1 +1,10 @@
+require 'spec_helper'
+ 
+describe "GET '/' - Greeting Form" do
+  it 'welcomes the user' do
+    visit '/'
+    expect(page.body).to include("Welcome!")
+  end
+end
+
 # Write your tests here!
